b656fb04e0e103ae800887f790097d2d38608197
Mathieu CHARDAN <mathieuchardan@gmail.com>
2023-11-14 16:29:03 +0100

    modifs thèmes + images

--- JS/Thèmes.js
+++ JS/Thèmes.js
@@ -7,3 +7,18 @@
 //   document.getElementById("titre1").classList.toggle("invisible");
 
 // });
+
+document.addEventListener("DOMContentLoaded", function() {
+    const images = document.querySelectorAll("#culture, #sport, #video, #music");
+    const textesCaches = document.querySelectorAll("#textecaché1, #textecaché2, #textecaché3, #textecaché4");
+
+    images.forEach(function(image, index) {
+        image.addEventListener("mouseenter", function() {
+            textesCaches[index].style.opacity = 1; // Apparaître
+        });
+
+        image.addEventListener("mouseleave", function() {
+            textesCaches[index].style.opacity = 0; // Disparaître
+        });
+    });
+});
