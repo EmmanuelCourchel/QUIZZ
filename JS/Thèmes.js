19d37bb074561a8e81bc68f25e402ff51413e746
Nicolas VSR <nicolas.vasseur07@gmail.com>
2023-11-14 14:02:28 +0100

    modif

---
+++ JS/Thèmes.js
@@ -0,0 +1,9 @@
+// const culture = document.getElementById("culture");
+// const sport = document.getElementById("sport");
+// const video = document.getElementById("Video");
+// const musique = document.getElementById("Musique");
+
+// culture.addEventListener("mouseover", function (e) {
+//   document.getElementById("titre1").classList.toggle("invisible");
+
+// });
